008ec2b7106ed3ca3691775d392982fae1d0b428
Wyatt Wilson <33297426+Wyatt-Wilson@users.noreply.github.com>
2023-04-23 13:33:00 -0400

    corrected index.js and package.json

--- app/index.js
+++ app/index.js
@@ -5,14 +5,13 @@ import { today } from "user-activity";
 import { me as appbit } from "appbit";
 import { preferences } from "user-settings";
 import { week } from "user-activity";
-//import { battery } from "power"; not in use at the moment
+import { battery } from "power";
 
 ////////////////////////////////////////////////////////////////////
 ///   ui elements     
 
 // steps
 const uiSteps = document.getElementById("labelSteps"); //today.adjusted.steps;
-//console.log(`${today.adjusted.steps}`); this prints the number of steps
 
 // cals
 const uiCals = document.getElementById("labelCals");
@@ -26,17 +25,12 @@ const uiHeartRate = document.getElementById("labelHeartRate");
 ////////////////////////////////////////////////////////////////////
 ///   debugging for general ui's
 if (HeartRateSensor) {
-  console.log("This device has a HeartRateSensor!");
   const hrm = new HeartRateSensor();
   hrm.addEventListener("reading", () => {
-    console.log(`Current heart rate: ${hrm.heartRate}`);
     uiHeartRate.text = `${hrm.heartRate}`; //implementation of heart rate text
   });
   hrm.start();
-} else {
-  console.log("This device does NOT have a HeartRateSensor!");
 }
-
 
 if (appbit.permissions.granted("access_activity")) {
    console.log(`${week.adjusted.activeZoneMinutes} AZM`);
@@ -48,44 +42,67 @@ if (appbit.permissions.granted("access_activity")) {
     console.log(`${today.adjusted.elevationGain} Floor(s)`);
   }
 }
-
+console.log(Math.floor(battery.chargeLevel) + "%");
 ////////////////////////////////////////////////////////////////////
-///    getting matching text for appropriate ui
+///    getting text to show for matching ui
 uiSteps.text = `${today.adjusted.steps}`;
 uiCals.text = `${today.adjusted.calories}`;
-uiDist.text = `${today.adjusted.distance}`;
-//uiAzm.text = `${today.adjusted.elevationGain}`;
+const num = `${today.adjusted.distance}`;
+num /= 1609; //converting meters to miles
+num = num.toFixed(2);
+uiDist.text = num;
 
-function zeroPad(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
+////////////////////////////////////////////////////////////////////
+///   Getting battery pngs to laod
+const imgReplace = document.getElementById("batteryIcon");
+
+if(Math.floor(battery.chargeLevel) > 75){ //If battery % is greater than 75 display the appropriate battery.png and so on...
+  imgReplace.href = "icons/battery100.png";
+} else if(Math.floor(battery.chargeLevel) > 50){
+  imgReplace.href = "icons/battery75.png";
+} else if(Math.floor(battery.chargeLevel) > 25){
+  imgReplace.href = "icons/battery50.png";
+} else if(Math.floor(battery.chargeLevel) > 10){
+  imgReplace.href = "icons/battery25.png";
+} else if(Math.floor(battery.chargeLevel) > 0){
+  imgReplace.href = "icons/battery10.png";
+};
+
+if(battery.charging == true){
+  imgReplace.href = "icons/batteryCharge.png";
+}
+console.log("The charger " + (battery.charging ? "is" : "is not") + " connected");
+//console.log("The charger " + (charger.connected ? "is" : "is not") + " connected");
   
 
 ////////////////////////////////////////////////////////////////////
 ///    default clock
-  // Update the clock every minute
-  clock.granularity = "minutes";
-  
-  // Get a handle on the <text> element
-  const myLabel = document.getElementById("myLabel");
-  
-  // Update the <text> element every tick with the current time
-  clock.ontick = (evt) => {
-    let today = evt.date;
-    let hours = today.getHours();
-    if (preferences.clockDisplay === "12h") {
-      // 12h format
-      hours = hours % 12 || 12;
-    } else {
-      // 24h format
-      hours = zeroPad(hours);
-    }
-    let mins = zeroPad(today.getMinutes());
-    myLabel.text = `${hours}:${mins}`;
+function zeroPad(i) {
+  if (i < 10) {
+    i = "0" + i;
   }
+  return i;
+}
+// Update the clock every minute
+clock.granularity = "minutes";
+
+// Get a handle on the <text> element
+const myLabel = document.getElementById("myLabel");
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = zeroPad(hours);
+  }
+  let mins = zeroPad(today.getMinutes());
+  myLabel.text = `${hours}:${mins}`;
+}
 
 
 ////////////////////////////////////////////////////////////////////
@@ -108,17 +125,17 @@ function tickHandler(evt) {
   txtDate.text = dateString;
 }
 
-  const monthsShort = [
-    "JAN",
-    "FEB",
-    "MAR",
-    "APR",
-    "MAY",
-    "JUN",
-    "JUL",
-    "AUG",
-    "SEP",
-    "OCT",
-    "NOV",
-    "DEC",
-  ];
+const monthsShort = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
